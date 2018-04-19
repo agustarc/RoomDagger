8a77f6621af3b23f2b23e9ccde5c8cc38d69d877
Leopold Baik <joongwon100@gmail.com>
2018-04-19 10:39:10 +0900

    Gradle setup

---
+++ app/src/main/kotlin/com/leopold/roomdagger/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.leopold.roomdagger
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * @author Leopold
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
